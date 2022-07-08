898287a538be2123354a021fafb75088f8705181
jamezzz123 <chinwokwuj@gmail.com>
2022-07-08 14:13:24 +0100

    refactor: permissions commit

--- pages/dashboard.vue
+++ pages/dashboard.vue
@@ -1,11 +1,32 @@
 <template>
-  <nuxt-child />
+  <div>
+    <nuxt-child v-if="!$store.state.auth.disablePage" />
+    <div v-else class="text-center m-auto mt-5">
+     <h2 class="text-capitalize"> you don't have permission to view this page</h2>
+      <p class="text-center paragraph-denied">
+        You do not have permission to access this page. please contact your
+        systems administrator.
+      </p>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
   layout: 'auth2',
-  middleware:'authenticated'
+  middleware: 'authenticated',
+  computed: {
+    pageDisabled() {
+      return this.$store.state.auth.disablePage
+    },
+  },
+
+  watch: {
+    '$route.path'() {
+      debugger
+      this.$store.commit('auth/SET_PAGE_DISABLED', false)
+    },
+  },
 }
 </script>
 
