e836de201098ff6df97ec2f73ee3bd79f8f4deda
Kinglemar <dimeji.ogunleye20@gmail.com>
2022-08-05 11:43:47 +0100

    Fix: Bug on Page change imaging obv and search by name

--- components/Dashboard/ImagingObservationSettings.vue
+++ components/Dashboard/ImagingObservationSettings.vue
@@ -7,13 +7,17 @@
         >
       </template>
       <template>
-        <TableComponent
+        <TableComponent @edit="edit($event)"
+          @page-changed="pageChange($event, filter)"
+          @delete="deleteItem($event)"
           :fields="fields"
           :pages="pages"
           :items="items"
           :busy="busy"
-          @edit="edit($event)"
-          @page-changed="pageChange(1)"
+           :perPage="filter.size"
+            :showBaseCount="trigger"
+          :currentPage="currentPage"
+           :totalRecords="totalRecords"
         >
         </TableComponent>
       </template>
@@ -21,8 +25,11 @@
     <DashboardModalAddImagingObservation
       @hide="editObj = {}"
       :edit-data="editObj"
-      @refresh="pageChange(1)"
+      @refresh="refreshMe"
     />
+
+    <DashboardModalDelete :editData="editObj"
+      @refresh="refreshMe"/>
   </div>
 </template>
 
@@ -34,6 +41,13 @@ export default {
   data() {
     return {
       editObj: {},
+        filter: {
+        size: 10,
+        name: '',
+      },
+      pages: 1,
+      currentPage: 1,
+      totalRecords: 0,
       fields: [
         {
           key: 'name',
@@ -42,13 +56,6 @@ export default {
           key: 'modality.name',
           label: 'Modality',
         },
-
-        //  {
-        //   key: 'bill_price',
-        // },
-        // {
-        //   key: 'bill_description',
-        // },
         {
           key: 'actions',
           label: '',
@@ -57,18 +64,28 @@ export default {
     }
   },
   async mounted() {
-    await this.pageChange(1)
+    await this.pageChange()
+  },
+   computed: {
+    trigger() {
+      if (this.items.length != 0) {
+        return true
+      }
+    },
   },
   methods: {
     searchMe(e){
-      this.pageChange(1, {name:e})
+      this.filter.name = e
+      this.pageChange(this.currentPage, this.filter)
     },
-    async pageChange(page = 1, e={}) {
+    async pageChange(page = 1, e={ size: 10, name: '' }) {
       try {
         this.busy = true
-        const data = await this.$api.imaging.getObservation({ page, ...e })
+        const data = await this.$api.imaging.getObservation({...e, page: page })
         this.items = data.results
         this.pages = data.total_pages
+        this.totalRecords = data.total_count
+        this.currentPage = data.current_page
         this.busy = false
       } catch (error) {
         console.log(error)
@@ -80,6 +97,14 @@ export default {
       this.editObj = { ...e , modality: e.modality.id}
       this.$bvModal.show('modal')
       console.log(e)
+    },
+    deleteItem(e){
+      console.log(e)
+      this.editObj = { ...e , modality: e.modality.id}
+      this.$bvModal.show('deleteModal')
+    },
+    refreshMe() {
+      this.pageChange(this.currentPage)
     },
   },
 }
