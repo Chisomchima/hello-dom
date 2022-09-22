a06f54884472b72b7d2c5b5d2f38d4c18fd1c943
Kinglemar <dimeji.ogunleye20@gmail.com>
2022-09-22 15:26:49 +0100

    Init: Patient reports

--- pages/dashboard/reports/laboratory/index.vue
+++ pages/dashboard/reports/laboratory/index.vue
@@ -38,7 +38,7 @@
                 @click="downloadEncLaboratory"
                 class="btn btn-sm btn-primary"
               >
-                <span class="mr-2">Download</span>
+                <span class="mr-2 pt-1">Download</span>
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
                   width="24"
