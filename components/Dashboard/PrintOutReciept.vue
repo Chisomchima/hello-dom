668cc112236268f0251fe6679a3e2372d9ee9a5c
Kinglemar <dimeji.ogunleye20@gmail.com>
2022-11-03 12:53:30 +0100

    Template logo

--- components/Dashboard/PrintOutReciept.vue
+++ components/Dashboard/PrintOutReciept.vue
@@ -138,7 +138,7 @@
           alt=""
         /> -->
         <!-- **************Mireva Medical************** -->
-        <!-- <svg
+        <svg
           version="1.0"
           xmlns="http://www.w3.org/2000/svg"
           width="180px"
@@ -4540,7 +4540,7 @@ c-31 27 -51 31 -61 15z"
               d="M6338 1033 c7 -3 16 -2 19 1 4 3 -2 6 -13 5 -11 0 -14 -3 -6 -6z"
             />
           </g>
-        </svg> -->
+        </svg>
       </div>
       <h3
         :style="printLayout ? { width: '20rem' } : { width: '48rem' }"
